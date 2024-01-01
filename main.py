20e59ec362f4a4a4434764980402390cecf93173
Veysel Aytekin <veysel.aytekin@outlook.de>
2024-01-02 00:08:17 +0100

    added a foto for GitHub ReadME

--- main.py
+++ main.py
@@ -10,3 +10,26 @@ from langchain.document_loaders import TextLoader
 
 # Streamlit Title
 st.title('Chatbot for Llama Papers')
+user_input = st.text_input("Please enter your question here")
+
+
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+
+openai_api_key = os.getenv('OPENAI_API_KEY')
+# Get the value of the 'OPENAI_API_KEY' environment variable.
+# I have a .gitignore file
+
+
+
+
+
+loader = TextLoader('papers.txt')
+
+papers = loader.load()
+
+embedding = OpenAIEmbeddings()  
+
+
