df4c098cb787ce5add3c9b4c6c2f829d84e3148d
Veysel Aytekin <veysel.aytekin@outlook.de>
2024-01-01 22:32:23 +0100

    assessment is done

---
+++ main.py
@@ -0,0 +1,12 @@
+import streamlit as st
+from langchain.vectorstores import Chroma
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.llms import OpenAI
+from langchain.chains import RetrievalQA
+from langchain.document_loaders import TextLoader
+
+
+
+# Streamlit Title
+st.title('Chatbot for Llama Papers')
